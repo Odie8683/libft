eac5ffecb0729ba8e6641fcb5eeb32019f5122b8
ozswar94 <jordan6heat@hotmail.fr>
2020-10-20 22:19:21 +0200

    second commit

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -1,6 +1,6 @@
 #include "../include/libft.h"
 
-void	*ft_memset(void *ptr, int value, size_t num)
+void *ft_memset(void *ptr, int value, size_t num)
 {
 	size_t i;
 	unsigned char *temp;
--- src/ft_putstr.c
+++ src/ft_putstr.c
@@ -1,6 +1,6 @@
 #include "../include/libft.h"
 
-void	ft_putstr(const char *str)
+void ft_putstr(const char *str)
 {
 	while(*str) {
 		write(1, str, 1);
